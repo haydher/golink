4c8b73cc48bdf285e9900cc91bc3a1f9db664c0e
haydher <haider0612@gmail.com>
2023-03-06 16:48:41 -0500

    adds background color

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import Repositories from "./components/Repositories/Repositories";
 
 function App() {
   return (
-    <div className="font-roboto w-11/12 sm:w-3/4 lg:w-1/2 m-auto">
-      <Header />
+    <div className="font-roboto text-slate-600 bg-slate-200">
+      <div className="w-11/12 sm:w-3/4 lg:w-1/2 m-auto">
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Repositories />} />
-        <Route path="/:org" element={<Repositories />} />
-        <Route path="/:org/commits/:repository" element={<Commits />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Repositories />} />
+          <Route path="/:org" element={<Repositories />} />
+          <Route path="/:org/commits/:repository" element={<Commits />} />
+        </Routes>
+      </div>
     </div>
   );
 }
