528e50b59cabb5c6e802511ebdee493be718d57f
haydher <haider0612@gmail.com>
2023-03-06 15:00:24 -0500

    adds routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Commits from "./components/Commits/Commits";
+import Header from "./components/Header";
+import Repositories from "./components/Repositories/Repositories";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-roboto w-11/12 sm:w-3/4 lg:w-1/2 m-auto">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Repositories />} />
+        <Route path="/:org" element={<Repositories />} />
+        <Route path="/:org/commits/:repository" element={<Commits />} />
+      </Routes>
     </div>
   );
 }
